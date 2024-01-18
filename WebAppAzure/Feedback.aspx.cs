64c3529b0fc68b12db0f6c550399029953dc0c90
Naveenn789 <naveengalla77@gmail.com>
2024-01-18 17:23:34 +0530

    Azure

---
+++ WebAppAzure/Feedback.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppAzure
+{
+    public partial class Feedback : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LblMsg.Visible = false;
+        }
+        protected void Btnsubmit_Click(object sender, EventArgs e)
+        {
+
+            LblMsg.Visible = true;
+            SqlConnection con = new SqlConnection("Server=tcp:naveen1204.database.windows.net,1433;Initial Catalog=SchoolDb;Persist Security Info=False;User ID=naveen789;Password=Prashanthi@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand("insert into FeedbackForm(EmployeeId,Name,Email,Rating) values(@EmployeeId,@Name,@mail,@rating) ", con);
+                cmd.Parameters.AddWithValue("@EmployeeId", int.Parse(Txtid.Text));
+                cmd.Parameters.AddWithValue("@Name", Txtname.Text);
+                cmd.Parameters.AddWithValue("@mail", Txtmail.Text);
+                cmd.Parameters.AddWithValue("@rating", int.Parse(Txtrating.Text));
+                con.Open();
+                cmd.ExecuteNonQuery();
+                LblMsg.Text = "Feedback Submitted!!!";
+
+
+            }
+            catch (Exception ex)
+            {
+                LblMsg.Text += "Error!!!" + ex.Message;
+            }
+            finally { con.Close(); }
+        }
+
+    }
+}
---
+++ WebAppAzure/obj/Release/Package/PackageTmp/Display.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/OurSite.Master" AutoEventWireup="true" CodeBehind="Display.aspx.cs" Inherits="WebAppAzure.Display" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <table class="w-100">
+    <tr>
+        <td><h1>Display</h1></td>
+    </tr>
+    <tr>
+        <td>
+            
+            <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:CommandField ShowSelectButton="True" />
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:SchoolDbConnectionString %>" ProviderName="<%$ ConnectionStrings:SchoolDbConnectionString.ProviderName %>" SelectCommand="SELECT [EmployeeId], [Name], [Email], [Rating] FROM [FeedbackForm]"></asp:SqlDataSource>
+            
+        </td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+    </tr>
+</table>
+</asp:Content>
